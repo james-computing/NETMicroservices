d605367c57a590f7ec99f071fe54aac8daaa1741
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-17 16:29:45 -0300

    Make http request for node port service

--- HttpRequests/Program.cs
+++ HttpRequests/Program.cs
@@ -4,8 +4,35 @@
     {
         public async static Task Main()
         {
+            await NodePort();
             //await CommandsServiceGet();
-            await CommandsServicePost();
+            //await CommandsServicePost();
+        }
+
+        public async static Task NodePort()
+        {
+            Console.WriteLine("Creating HTTP client...");
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Making HTTP request...");
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("http://localhost:30722/api/Platforms");
+                Console.WriteLine("Received response...");
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Status: {response.StatusCode}");
+                    string str = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Response: {str}");
+                }
+                else
+                {
+                    Console.WriteLine($"Status: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         // Worked
