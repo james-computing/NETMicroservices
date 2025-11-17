b37e2ab5f43bde6e9641d001a3054c921deb70ce
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-17 16:20:54 -0300

    Make POST request for Commands service

--- HttpRequests/Program.cs
+++ HttpRequests/Program.cs
@@ -4,7 +4,8 @@
     {
         public async static Task Main()
         {
-            await CommandsServiceGet();
+            //await CommandsServiceGet();
+            await CommandsServicePost();
         }
 
         // Worked
@@ -29,6 +30,35 @@
                 }
             }
             catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        // Worked
+        public async static Task CommandsServicePost()
+        {
+            Console.WriteLine("Creating HTTP client...");
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Creating content of POST request");
+            HttpContent content = new StringContent("nothing");
+            Console.WriteLine("Making HTTP request...");
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync("https://localhost:7178/api/CommandsForPlatforms", content);
+                Console.WriteLine("Received response...");
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Status: {response.StatusCode}");
+                    string str = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Response: {str}");
+                }
+                else
+                {
+                    Console.WriteLine($"Status: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
             }
