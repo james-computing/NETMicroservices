59a9cb6cd6e62f7bc6a0fb24cbaaf331529f0bbc
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-17 16:15:30 -0300

    Create console application to make http requests

---
+++ HttpRequests/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HttpRequests
+{
+    internal class Program
+    {
+        public async static Task Main()
+        {
+            await CommandsServiceGet();
+        }
+
+        public async static Task CommandsServiceGet()
+        {
+            Console.WriteLine("Creating HTTP client...");
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Making HTTP request...");
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://localhost:7178/api/CommandsForPlatforms");
+                Console.WriteLine("Received response...");
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Status: {response.StatusCode}");
+                    string str = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Response: {str}");
+                }
+                else
+                {
+                    Console.WriteLine($"Status: {response.StatusCode}");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
