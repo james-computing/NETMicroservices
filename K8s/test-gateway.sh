4ed553424bb3f49cac434105adfd9f0a0b45443f
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-19 14:34:53 -0300

    Make another POST request in the gateway test

--- K8s/test-gateway.sh
+++ K8s/test-gateway.sh
@@ -5,3 +5,4 @@ curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/CommandsForPlatforms
 
 # POST
 curl --resolve acme.com:80:127.0.0.1 --json '{"Name": "name", "Publisher": "publisher", "Cost": "cost"}' http://acme.com/api/Platforms
+curl --resolve acme.com:80:127.0.0.1 -X POST http://acme.com/api/CommandsForPlatforms
