c2127ee638c4c0bed025cdaf0485a506daf920a8
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-19 14:19:35 -0300

    Add a POST request for the gateway test

--- K8s/test-gateway.sh
+++ K8s/test-gateway.sh
@@ -1,2 +1,6 @@
+# GET
 curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/Platforms
 curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/CommandsForPlatforms
+
+# POST
+curl --resolve acme.com:80:127.0.0.1 --json '{"Name": "name", "Publisher": "publisher", "Cost": "cost"}' http://acme.com/api/Platforms
