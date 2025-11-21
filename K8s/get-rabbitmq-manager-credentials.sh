f864923eb328be98a53b0d1b667c7765e96b1d0b
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-21 18:39:24 -0300

    Create a script to see the RabbitMQ Manager credentials

---
+++ K8s/get-rabbitmq-manager-credentials.sh
@@ -0,0 +1,4 @@
+username="$(kubectl get secret hello-world-default-user -o jsonpath='{.data.username}' | base64 --decode)"
+echo "username: $username"
+password="$(kubectl get secret hello-world-default-user -o jsonpath='{.data.password}' | base64 --decode)"
+echo "password: $password"
