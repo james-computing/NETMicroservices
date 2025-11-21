28e07bba3321951959a8a18c12a292147c7723e2
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-20 21:42:42 -0300

    Create a repository for the Commands service
    The repository is used for accessing the database

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,19 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        public bool SaveChanges();
+
+        // Platforms
+        public IEnumerable<Platform> GetAllPlatforms();
+        public void CreatePlatform(Platform platform);
+        public bool PlatformExists(int platformId);
+
+        // Commands
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        public Command? GetCommand(int platformId, int commandId);
+        public void CreateCommand(int platformId, Command command);
+    }
+}
