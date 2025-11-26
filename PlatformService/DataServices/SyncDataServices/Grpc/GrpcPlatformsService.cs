041bd4b03aa0919c8076e4883747216a7af355d0
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-26 16:25:14 -0300

    Create a class GrpcPlatformsService which uses gRPC to send all Platforms from the Platforms service

---
+++ PlatformService/DataServices/SyncDataServices/Grpc/GrpcPlatformsService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Grpc.Core;
+using PlatformService.Data;
+using PlatformService.Models;
+using PlatformsService; // for PlatformResponse and etc
+using static PlatformsService.GRPCPlatform;
+
+namespace PlatformService.DataServices.SyncDataServices.Grpc
+{
+    public class GrpcPlatformsService : GRPCPlatformBase
+    {
+        private readonly IPlatformRepo _repo;
+        private readonly IMapper _mapper;
+
+        public GrpcPlatformsService(IPlatformRepo repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        public override async Task<PlatformResponse> GetAllPlatforms(GetAllRequest request, ServerCallContext context)
+        {
+            PlatformResponse response = new PlatformResponse();
+            IEnumerable<Platform> platforms = _repo.GetAllPlatforms();
+            foreach(Platform platform in platforms)
+            {
+                response.Platform.Add(_mapper.Map<GRPCPlatformModel>(platform));
+            }
+            return response;
+        }
+    }
+}
