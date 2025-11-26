e97a5b1ebe76724f3cc860a0d359843481f9b42e
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-26 20:22:42 -0300

    Update api tests

--- K8s/test-gateway.sh
+++ K8s/test-gateway.sh
@@ -6,10 +6,11 @@ curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/Platforms/1
 curl --resolve acme.com:80:127.0.0.1 --json '{"Name": "name", "Publisher": "publisher", "Cost": "cost"}' http://acme.com/api/Platforms
 
 # Commands service
+# Note that the platformId is the Id, not the ExternalId, in the Platform model of the Commands service
 # GET
 curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/CommandsPlatforms
-curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/Commands/platformId/1010
-curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/Commands/platformId/1010/commandId/1
+curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/Commands/platformId/1
+curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/Commands/platformId/1/commandId/1
 # POST
 curl --resolve acme.com:80:127.0.0.1 -X POST http://acme.com/api/CommandsPlatforms
-curl --resolve acme.com:80:127.0.0.1 --json '{"HowTo": "Push a docker container to Hub", "CommandLine": "docker push <name of container>"}' http://acme.com/api/Commands/platformId/1010
+curl --resolve acme.com:80:127.0.0.1 --json '{"HowTo": "Push a docker container to Hub", "CommandLine": "docker push <name of container>"}' http://acme.com/api/Commands/platformId/1
