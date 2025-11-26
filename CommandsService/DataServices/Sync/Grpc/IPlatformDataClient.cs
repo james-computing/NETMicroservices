d56eff4c2966c9f3d5eb995a12b1f37b639ad47a
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-26 16:35:01 -0300

    Make a class PlatformDataClient that uses gRPC to get the platforms from the Platforms service

---
+++ CommandsService/DataServices/Sync/Grpc/IPlatformDataClient.cs
@@ -0,0 +1,9 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.DataServices.Sync.Grpc
+{
+    public interface IPlatformDataClient
+    {
+        public IEnumerable<Platform>? ReturnAllPlatform();
+    }
+}
---
+++ CommandsService/DataServices/Sync/Grpc/PlatformDataClient.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using CommandsService.Models;
+using Grpc.Net.Client;
+using PlatformsService;
+using static PlatformsService.GRPCPlatform;
+
+namespace CommandsService.DataServices.Sync.Grpc
+{
+    public class PlatformDataClient : IPlatformDataClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public PlatformDataClient(IConfiguration configuration, IMapper mapper)
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<Platform>? ReturnAllPlatform()
+        {
+            string grpcAddress = _configuration.GetValue<string>("GrpcPlatform")!;
+            Console.WriteLine($"--> Calling GRPC Service {grpcAddress}");
+            GrpcChannel channel = GrpcChannel.ForAddress(grpcAddress);
+            GRPCPlatformClient client = new GRPCPlatformClient(channel);
+            GetAllRequest request = new GetAllRequest();
+
+            try
+            {
+                PlatformResponse response = client.GetAllPlatforms(request);
+                return _mapper.Map<IEnumerable<Platform>>(response.Platform);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"--> Couldn't call GRPC server: {ex.Message}");
+            }
+            return null;
+        }
+    }
+}
