a9d1550485d09603c76f4f08f85ea6b02926d228
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-26 16:38:48 -0300

    Make a class PrepDb that gets the platforms from the Platforms service and add the data to the Commands service database
    It uses the IPlatformDataClient to get the platforms, which is using gRPC.

---
+++ CommandsService/Data/PrepDb.cs
@@ -0,0 +1,45 @@
+﻿using CommandsService.DataServices.Sync.Grpc;
+using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            // Create a scope, so we can use GRPC
+            using (IServiceScope serviceScope = app.ApplicationServices.CreateScope())
+            {
+                // Get the services
+                IPlatformDataClient? grpcClient = serviceScope.ServiceProvider.GetService<IPlatformDataClient>();
+                ICommandRepo? repo = serviceScope.ServiceProvider.GetService<ICommandRepo>();
+                if (grpcClient == null || repo == null)
+                {
+                    Console.WriteLine("Failed to get service in PrepDb.PrepPopulation");
+                    return;
+                }
+
+                // Use gRPC to get the platforms from the Platforms service
+                IEnumerable<Platform>? platforms = grpcClient.ReturnAllPlatform();
+                // If the gRPC succeeded, store the data in the Commands service database
+                if(platforms != null)
+                {
+                    SeedData(repo,platforms);
+                }
+            }
+        }
+
+        private static void SeedData(ICommandRepo repo, IEnumerable<Platform> platforms)
+        {
+            Console.WriteLine("--> Seeding new platforms...");
+            foreach(Platform platform in platforms)
+            {
+                if(repo.ExternalPlatformExists(platform.Id) == false)
+                {
+                    repo.CreatePlatform(platform);
+                }
+                repo.SaveChanges();
+            }
+        }
+    }
+}
