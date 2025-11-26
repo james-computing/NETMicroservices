911e57c907ac5994680b9e54693a652e735cff80
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-26 00:14:31 -0300

    Add more tests for the API

--- K8s/test-gateway.sh
+++ K8s/test-gateway.sh
@@ -1,8 +1,15 @@
+# Platforms service
 # GET
 curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/Platforms
 curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/Platforms/1
-curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/CommandsPlatforms
-
-# POST
+#POST
 curl --resolve acme.com:80:127.0.0.1 --json '{"Name": "name", "Publisher": "publisher", "Cost": "cost"}' http://acme.com/api/Platforms
+
+# Commands service
+# GET
+curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/CommandsPlatforms
+curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/Commands/platformId/1010
+curl --resolve acme.com:80:127.0.0.1 http://acme.com/api/Commands/platformId/1010/commandId/1
+# POST
 curl --resolve acme.com:80:127.0.0.1 -X POST http://acme.com/api/CommandsPlatforms
+curl --resolve acme.com:80:127.0.0.1 --json '{"HowTo": "Push a docker container to Hub", "CommandLine": "docker push <name of container>"}' http://acme.com/api/Commands/platformId/1010
